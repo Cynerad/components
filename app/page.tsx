58dbc55deb59678d6068169fd54cf8f2c236a2cf
javad <javadgharibi123@gmail.com>
2026-06-23 08:08:15 +0330

    chore : add password input

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 import Container from "@/components/ui/container";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { PasswordInput } from "@/registry/ui/password-input";
 
 export default function Home() {
-  return <Container>just simple playground</Container>;
+  return (
+    <Container>
+      <Field className="max-w-sm">
+        <FieldLabel htmlFor="password">Input</FieldLabel>
+        <PasswordInput name="password" id="password" />
+      </Field>
+    </Container>
+  );
 }
---
+++ registry/ui/password-input.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { EyeClosed, EyeOffIcon } from "lucide-react";
+
+import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from "@/components/ui/input-group";
+import { ComponentProps, useState } from "react";
+
+type PasswordInputType = {
+  name: string;
+} & ComponentProps<typeof InputGroupInput>;
+
+export function PasswordInput({ name, ...props }: PasswordInputType) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <InputGroup>
+      <InputGroupInput name={name} type={showPassword ? "text" : "password"} {...props} />
+      <InputGroupAddon align="inline-end">
+        <InputGroupButton onClick={() => setShowPassword((p) => !p)}>{showPassword ? <EyeClosed /> : <EyeOffIcon />}</InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
+  );
+}
