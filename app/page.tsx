5b6e50c258e407eac26ce92c4b9e36671baa6412
javad <javadgharibi123@gmail.com>
2026-06-23 07:48:40 +0330

    chore : add back to top button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,7 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import Container from "@/components/ui/container";
-import { Rating } from "@/registry/ui/rating";
 
 export default function Home() {
-  return (
-    <Container>
-      <div className="max-w-[400px]">
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            const data = new FormData(event.currentTarget);
-            console.log(data.get("ratings-product"));
-          }}
-        >
-          <Rating name="ratings-product" variant="default" />
-          <Button type="submit">submit</Button>
-        </form>
-      </div>
-    </Container>
-  );
+  return <Container>just simple playground</Container>;
 }
